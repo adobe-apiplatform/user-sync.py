bc85540c6838f41e3d72acf9ae52b72d71a1350a
Andrew Dorton <adorton@adobe.com>
2016-06-14 21:07:17 -0600

    refactor ldap connection to separate function (makes it easier to test)

--- aedash_connector/input.py
+++ aedash_connector/input.py
@@ -31,20 +31,14 @@ def from_csv(reader):
         yield outrec
 
 
-def from_ldap(host, username, pw, domain):
+def from_ldap(con, domain):
     """
     Get directory users from LDAP query
 
-    :param host: LDAP Host
-    :param username: LDAP User
-    :param pw: LDAP Domain
+    :param con: LDAP con object
     :param domain: Enterprise Domain
     :return: Generator yields one user record per iteration
     """
-    con = ldap.initialize(host)
-    con.protocol_version = 3
-    con.set_option(ldap.OPT_REFERRALS, 0)
-    con.simple_bind_s(username, pw)
 
     base_dn = "dc=ccestestdomain,dc=com"
     fltr = "(&(objectClass=person)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
@@ -68,3 +62,20 @@ def from_ldap(host, username, pw, domain):
                 if m:
                     outrec['groups'].append(m.group('group'))
         yield outrec
+
+
+def make_ldap_con(host, username, pw):
+    """
+    Make an LDAP con object
+
+    :param host: LDAP Host
+    :param username: LDAP User
+    :param pw: LDAP Domain
+    :return: LDAP con object
+    """
+
+    con = ldap.initialize(host)
+    con.protocol_version = 3
+    con.set_option(ldap.OPT_REFERRALS, 0)
+    con.simple_bind_s(username, pw)
+    return con
