0ebce9cdfa10cda76eba51ca84e23e9b5ebe60e7
David Yue <davidy@ensemble.com>
2016-11-15 18:02:04 -0800

    Added in missing source for credential_manager.

---
+++ aedash/sync/credential_manager.py
@@ -0,0 +1,12 @@
+
+UMAPI_CREDENTIAL_TYPE = "Adobe_UMAPI"
+DIRECTORY_CREDENTIAL_TYPE = "Directory"
+
+def get_credentials(credential_type, credential_id, **kwArgs):
+    '''
+    :type credential_type: str
+    :type id: str
+    :rtype dict
+    '''
+    return None
+    
