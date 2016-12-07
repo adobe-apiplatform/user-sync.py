ee6ed93d7aca3513e0fba18244796a20ef5d79bc
jamesg <jamesg@ensemble.com>
2016-12-07 15:58:51 +0000

    add config tests

--- tests/config_test.py
+++ tests/config_test.py
@@ -9,33 +9,29 @@ from aedash.sync.config import ConfigLoader
 
 
 class ConfigLoaderTest(unittest.TestCase):
+
     @mock.patch('os.path.isfile')
-    @mock.patch('aedash.sync.config.DictConfig.get_value')
     @mock.patch('aedash.sync.config.ConfigLoader.load_from_yaml')
-    @mock.patch('aedash.sync.config.ConfigLoader.get_dict_from_sources')
-    def test_get_dashboard_options_for_owning(self,mock_get_dict,mock_yaml_load,mock_get_value,mock_isfile,):
-
-        conf_load = ConfigLoader({})
-
-
+    def setUp(self,mock_yaml,mock_isfile):
         mock_isfile.return_value = True
-        conf_load.get_dashboard_options_for_owning()
+        self.conf_load = ConfigLoader({})
 
-        mock_isfile.return_value = False
-        conf_load.get_dashboard_options_for_owning()
-        self.assertRaises(aedash.sync.error.AssertionException,)
 
-        # def get_dashboard_options_for_owning(self):
-        #     owning_config_filename = DEFAULT_DASHBOARD_OWNING_CONFIG_FILENAME
-        #
-        #     owning_config = None
-        #     dashboard_config = self.main_config.get_dict_config('dashboard', True)
-        #     if (dashboard_config != None):
-        #         owning_config = dashboard_config.get_list('owning', True)
-        #     owning_config_sources = self.as_list(owning_config)
-        #     if (os.path.isfile(owning_config_filename)):
-        #         owning_config_sources.append(owning_config_filename)
-        #     owning_config_sources.append({
-        #         'test_mode': self.options['test_mode']
-        #     })
-        #     return self.create_dashboard_options(owning_config_sources, 'owning_dashboard')
+    @mock.patch('aedash.sync.config.DictConfig.get_value')
+    @mock.patch('aedash.sync.config.ConfigLoader.get_dict_from_sources')
+    @mock.patch('aedash.sync.config.ConfigLoader.create_dashboard_options')
+    def test_get_dashboard_options_creates_dashboard(self,mock_create_dash, mock_dict, mock_value):
+        ret_value = 'create dash'
+        mock_create_dash.return_value = ret_value
+        self.assertEquals(self.conf_load.get_dashboard_options_for_owning(),ret_value, "Returned with created dashboard")
+        self.assertEquals(mock_create_dash.call_count,1, "Get dashboard options calls create dashboard options")
+
+
+    @mock.patch('aedash.sync.config.ConfigLoader.get_directory_connector_configs')
+    @mock.patch('aedash.sync.config.ConfigLoader.get_dict_from_sources')
+    def test_get_directory_connector_options(self,mock_dict, mock_connector_conf):
+        self.assertEquals(mock_dict.call_count, 3, "connector options, source filters and credentials are loaded")
+
+
+    if __name__ == "__main__":
+        unittest.main()
