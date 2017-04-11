800a0ccfb576439cbf65e23d2a3dc3bcf18f4be1
Kevin Bhunut <bhunut@adobe.com>
2017-04-10 17:03:12 -0700

    Added Tests for Group Filter Format for Okta Connector

--- tests/connector/directory_okta_test.py
+++ tests/connector/directory_okta_test.py
@@ -76,3 +76,62 @@ class TestOktaErrors(unittest.TestCase):
 
         self.assertRaises(AssertionException,
                           connector_load_users_and_groups, self.directory, ['group1', 'group2'], [])
+
+
+class TestOktaGroupFilter(unittest.TestCase):
+    def setUp(self):
+        class MockResponse:
+            def __init__(self, status_code, data):
+                self.status_code = status_code
+                self.text = json.dumps(data)
+
+        self.mock_response = MockResponse
+
+        self.orig_directory_init = OktaDirectoryConnector.__init__
+
+        OktaDirectoryConnector.__init__ = mock.Mock(return_value=None)
+        directory = OktaDirectoryConnector({})
+
+        directory.logger = mock.create_autospec(logging.Logger)
+        directory.groups_client = okta.UserGroupsClient('example.com', 'xyz')
+
+        self.directory = directory
+
+    def tearDown(self):
+        OktaDirectoryConnector.__init__ = self.orig_directory_init
+
+    @mock.patch('okta.framework.ApiClient.requests')
+    def test_success_group_filter(self, mock_requests):
+        # This test success group filter with valid Group
+        # This should return Okta GroupsClient Object and Contained property Profile.
+
+        mock_requests.get.return_value = self.mock_response(200, [
+            {"id": "00g9sq2jcqpk3LwCV0h7",
+             "objectClass": ["okta:user_group"], "type": "OKTA_GROUP",
+             "profile": {"name": "Group 1", "description": "null"}}])
+        directory = self.directory
+        directory.options = {'source_filters': {}, 'all_users_filter': None, 'group_filter_format': '{group}'}
+        result = directory.find_group("Group 1")
+        self.assertEqual(result.profile.name, "Group 1")
+
+    @mock.patch('okta.framework.ApiClient.requests')
+    def test_bad_group_filter_1(self, mock_requests):
+        # Test scenario where Group Filter is bad
+        # This will not return any group because it can't be find.
+
+        mock_requests.get.return_value = self.mock_response(200, [])
+        directory = self.directory
+        directory.options = {'source_filters': {}, 'all_users_filter': None,
+                             'group_filter_format': '(BADFILTER){group}'}
+        result = directory.find_group("Group 1")
+        self.assertEqual(result, None)
+
+    def test_bad_group_filter_2(self):
+        # Test another scenario of bad group filter - {groupA} instead of {group}
+        # This should throw an exception
+
+        directory = self.directory
+        directory.options = {'source_filters': {}, 'all_users_filter': None,
+                             'group_filter_format': '{groupA}'}
+        self.assertRaises(AssertionException, directory.find_group, "Group 1")
+
