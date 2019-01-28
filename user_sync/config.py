34f8bf0fc04cdfd8d981f5c393f217e73f7ad879
shasi <shjena@adobe.com>
2019-01-28 22:24:21 +0530

    Log a meaningful error if Okta connector isn't invoked correctly
    removed connectors_config.accessed_keys loop and checking with container value comparision with JSON object
    
    https://github.com/adobe-apiplatform/user-sync.py/issues/416

--- user_sync/config.py
+++ user_sync/config.py
@@ -324,10 +324,9 @@ class ConfigLoader(object):
         options = {}
         connectors_config = self.get_directory_connector_configs()
 
-        if connectors_config is not None:
-            for accessed_keys_type in connectors_config.accessed_keys:
-                if (accessed_keys_type =='okta') and (connector_name == 'ldap'):
-                    raise AssertionException("Failed to match request : for Okta Connector type receiving LDAP as input type from commnand line")
+        if ( 'okta' in connectors_config.value) and (connector_name == 'ldap'):
+            if connectors_config.accessed_keys is not None:
+                raise AssertionException("Failed to match request : for Okta Connector type receiving LDAP as input type from commnand line")
 
         if connectors_config is not None:
             connector_item = connectors_config.get_list(connector_name, True)
