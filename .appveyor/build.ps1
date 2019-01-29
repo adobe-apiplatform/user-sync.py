db1abf1384b87f0524b6bfd01a297c8f1eb7209c
Andrew Dorton <adorton@adobe.com>
2019-01-29 09:46:15 -0700

    fix windows archives

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -15,10 +15,10 @@ Get-Command python
 $pyver=$(python -V 2>&1) -replace "Python ","py" -replace "\.",""
 echo "pyver: ${pyver}"
 7z a -ttar "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar" user-sync.pex
-7z a -tgzip "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar" "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar.gz"
-7z a "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar.gz" user-sync.pex
+7z a -tgzip "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar.gz" "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.tar"
 7z a "user-sync-${env:APPVEYOR_REPO_TAG_NAME}-win64-${pyver}.zip" user-sync.pex
 cd ..
 7z a -ttar -r release\examples.tar examples
 7z a -tgzip release\examples.tar.gz release\examples.tar
+7z a -r release\examples.zip examples\
 dir release
