7c3202e594213df2e19de773a81e16edf3a7fd27
Andrew Dorton <adorton@adobe.com>
2019-05-01 14:05:44 -0600

    see if we can fix setup.py error on centOS py36

--- .build/.travis/build-py36-centos.sh
+++ .build/.travis/build-py36-centos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 virtualenv venv -p /usr/bin/python3.6
 source venv/bin/activate
+python -m pip install --upgrade pip
 pip install external/okta-0.0.3.1-py2.py3-none-any.whl
 pip install -e .
 pip install -e .[test]
