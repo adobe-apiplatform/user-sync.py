b0b07aedfa4b606c58fe9572e88b5deab9173882
Unknown <danimaejanssen@gmail.com>
2019-05-03 11:12:11 -0500

    directory.py test finished

--- tests/test_connector_directory.py
+++ tests/test_connector_directory.py
@@ -1,5 +1,5 @@
-import os
 import pytest
+
 from user_sync.connector.directory import DirectoryConnector
 from user_sync.error import AssertionException
 
@@ -8,7 +8,6 @@ from user_sync.error import AssertionException
 def get_implementation():
     dc_mod_name = "user_sync.connector.directory_ldap"
     return __import__(dc_mod_name, fromlist=[''])
-
 
 def test_required_functions(get_implementation):
 
@@ -19,24 +18,3 @@ def test_required_functions(get_implementation):
     base_impl = get_implementation
     del base_impl.connector_initialize
     pytest.raises(AssertionException, DirectoryConnector, base_impl)
-
-
-
-def test_initialize():
-
-
-
-    print()
-
-    # use mock implementation above to call connector_initialize
-    # assert self.state set to properly initialized instance of connector
-
-    pass
-
-def test_load_users_and_groups():
-
-    # not sure this needs a real test, since all it's doing is acting as a pass through
-    # maybe we can validate the methods are being called correctly via connector_load_users_and_groups
-    # since they are being executed via self.state 
-
-    pass
