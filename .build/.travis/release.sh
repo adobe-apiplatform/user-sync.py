8e580c8f0bce8a2a0128024e9c4995cd8d1e7d68
Andrew Dorton <adorton@adobe.com>
2019-05-14 18:56:43 +0000

    add standalone build for ubuntu

--- .build/.travis/release.sh
+++ .build/.travis/release.sh
@@ -2,9 +2,17 @@
 PY_VER=$(2>&1 python -V | sed -e 's/Python /py/g' | sed -e 's/\.//g')
 echo ${PY_VER}
 cd dist
-tar czf "user-sync-${TRAVIS_TAG}-${IMG}-${PY_VER}.tar.gz" user-sync
-ls -al
-cd ..
-mkdir -p release
-mv dist/*.tar.gz release/
-ls -al release/
+if [ "$BUILD_TARGET" = "pex" ]; then
+  tar czf "user-sync-${TRAVIS_TAG}-${IMG}-${PY_VER}.tar.gz" user-sync
+  ls -al
+  cd ..
+  mkdir -p release
+  mv dist/*.tar.gz release/
+  ls -al release/
+else
+  mv user-sync "user-sync-${TRAVIS_TAG}-${IMG}-${PY_VER}"
+  cd ..
+  mkdir -p release
+  mv dist/* release/
+  ls -al release/
+fi
