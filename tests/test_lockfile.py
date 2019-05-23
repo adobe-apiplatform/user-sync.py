284c01daa14c5f6115baf5138b87f6e65b03a56f
Sonam <sxc30210@ucmo.edu>
2019-05-23 16:39:32 -0500

    Finish Lockfile Test

--- tests/test_lockfile.py
+++ tests/test_lockfile.py
@@ -2,49 +2,37 @@ import os
 import pytest
 import user_sync.lockfile as lock
 
+
 @pytest.fixture
-def get_lock_filepath():
+def lock_filepath():
     return os.path.join(os.getcwd(), 'lockfile_test')
 
-def test_set_lock():
 
-    # use to set lock file
-    # assert lock file exists
-    # assert that PID in lockfile matches this process PID
-    pass
+def test_set_lock(lock_filepath):
+    plock1 = lock.ProcessLock(lock_filepath)
+    assert plock1.set_lock() is True
+    os.remove(lock_filepath)
 
 
-def test_is_locked(get_lock_filepath):
-    test_path = get_lock_filepath
-
-    plock = lock.ProcessLock("./randompath/lockfile")
+def test_is_locked(lock_filepath):
+    plock = lock.ProcessLock(lock_filepath)
     assert plock.is_locked() is False
 
-    plock.path = test_path
-    with open(test_path, 'w') as f:
+
+    plock.path = lock_filepath
+    with open(lock_filepath, 'w') as f:
         f.write("")
     assert plock.is_locked() is False
 
-    with open(test_path, 'w') as f:
+    with open(lock_filepath, 'w') as f:
         f.write("dfsdf")
     pytest.raises(ValueError, plock.is_locked)
 
-    with open(test_path, 'w') as f:
-        f.write("1234")
-    assert plock.is_locked() is False
+def test_unlock(lock_filepath):
+    plock2 = lock.ProcessLock(lock_filepath)
+    assert plock2.set_lock()
+    assert plock2.is_locked()
 
-    # with open(test_path, 'w') as f:
-    #     f.write(str(os.getpid()))
-    #assert plock.is_locked() is True
-
-    # any other cases that make sense ///
-
-    os.remove(test_path)
-
-def test_unlock():
-
-    # run setlock
-    # verify file exists
-    # run unlock
-    # assert is_locked = false
-    pass
+    plock2.unlock()
+    assert not plock2.is_locked()
+    assert not os.path.exists(lock_filepath)
