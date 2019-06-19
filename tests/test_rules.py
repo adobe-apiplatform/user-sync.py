430cd4436be14085f809a8adccd7b21889e086d0
Aishu <aishwaryakonduri92@gmail.com>
2019-06-19 10:44:03 -0500

    Added test_stray_key_map method in test_rules.py to test read_stray_key_map method from rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,8 +1,54 @@
-import os
+import mock
 import pytest
 
-
-def test_placeholder():
-    pass
+from user_sync.rules import RuleProcessor
 
 
+@pytest.fixture
+def rule_processor(caller_options):
+    return RuleProcessor(caller_options)
+
+
+@pytest.fixture
+def caller_options():
+    return {'adobe_group_filter': None, 'after_mapping_hook': None, 'default_country_code': 'US',
+            'delete_strays': False, 'directory_group_filter': None, 'disentitle_strays': False, 'exclude_groups': [],
+            'exclude_identity_types': ['adobeID'], 'exclude_strays': False, 'exclude_users': [],
+            'extended_attributes': None, 'process_groups': True, 'max_adobe_only_users': 200,
+            'new_account_type': 'federatedID', 'remove_strays': True, 'strategy': 'sync',
+            'stray_list_input_path': None, 'stray_list_output_path': None,
+            'test_mode': True, 'update_user_info': False, 'username_filter_regex': None,
+            'adobe_only_user_action': ['remove'], 'adobe_only_user_list': None,
+            'adobe_users': ['all'], 'config_filename': 'tests/fixture/user-sync-config.yml',
+            'connector': 'ldap', 'encoding_name': 'utf8', 'user_filter': None,
+            'users': None, 'directory_connector_type': 'csv',
+            'directory_connector_overridden_options': {'file_path': '../tests/fixture/remove-data.csv'},
+            'adobe_group_mapped': False, 'additional_groups': []}
+
+
+@mock.patch('user_sync.helper.CSVAdapter.read_csv_rows')
+def test_stray_key_map(csv_reader, rule_processor):
+    csv_mock_data = [{'type': 'adobeID', 'username': 'removeuser2@seaofcarag.com', 'domain': 'seaofcarag.com'},
+                     {'type': 'federatedID', 'username': 'removeuser@seaofcarag.com', 'domain': 'seaofcarag.com'},
+                     {'type': 'enterpriseID', 'username': 'removeuser3@seaofcarag.com', 'domain': 'seaofcarag.com'}]
+    csv_reader.return_value = csv_mock_data
+    rule_processor.read_stray_key_map('')
+    actual_value = rule_processor.stray_key_map
+    expected_value = {None: {'federatedID,removeuser@seaofcarag.com,': None,
+                             'enterpriseID,removeuser3@seaofcarag.com,': None,
+                             'adobeID,removeuser2@seaofcarag.com,': None}}
+
+    assert expected_value == actual_value
+
+    # Added secondary umapi value
+    csv_mock_data = [{'type': 'adobeID', 'username': 'remo@carag.com', 'domain': 'carag.com', 'umapi': 'secondary'},
+                     {'type': 'federatedID', 'username': 'removeuser@seaofcarag.com'},
+                     {'type': 'enterpriseID', 'username': 'removeuser3@seaofcarag.com', 'domain': 'seaofcarag.com'}]
+    csv_reader.return_value = csv_mock_data
+    rule_processor.read_stray_key_map('')
+    actual_value = rule_processor.stray_key_map
+    expected_value = {'secondary': {'adobeID,remo@carag.com,': None},
+                      None: {'federatedID,removeuser@seaofcarag.com,': None,
+                             'enterpriseID,removeuser3@seaofcarag.com,': None,
+                             'adobeID,removeuser2@seaofcarag.com,': None}}
+    assert expected_value == actual_value
