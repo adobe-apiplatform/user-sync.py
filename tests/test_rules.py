db8f2c4c70fdba14451c6186bdf4107318d4e607
Saba <sabanawafaslam@gmail.com>
2019-07-02 13:33:04 -0500

    I compare test_log_after_mapping_hook_scope  attributes before call and after call

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -2,12 +2,21 @@ import os
 import pytest
 import pytest
 import logging
+import re
+import yaml
+from six import StringIO
 
 from user_sync.rules import RuleProcessor
 
 
 def test_log_after_mapping_hook_scope(log_stream):
     stream, logger = log_stream
+
+    def compare_attr(text, target):
+        s = yaml.safe_load(re.search('{.+}', text).group())
+        for attr in s:
+            if s[attr] != 'None':
+                assert s[attr] == target[attr]
 
     state = {
         'source_attributes': {
@@ -35,14 +44,25 @@ def test_log_after_mapping_hook_scope(log_stream):
 
     ruleprocessor.after_mapping_hook_scope = state
     ruleprocessor.log_after_mapping_hook_scope(before_call=True)
-
     stream.flush()
-    x = stream.getvalue()
+    x = stream.getvalue().split('\n')
+
+    assert len(x[2]) < 32
+    assert len(x[4]) < 32
+    compare_attr(x[1], state['source_attributes'])
+    compare_attr(x[3], state['target_attributes'])
 
     state['target_groups'] = {'One'}
-    state['target_attributes'] =  {'firstname' : 'John'}
-    state['source_attributes'] = {'sn': 'David'}
+    state['target_attributes']['firstname'] = 'John'
+    state['source_attributes']['sn'] = 'David'
     state['source_groups'] = {'One'}
 
     ruleprocessor.after_mapping_hook_scope = state
     ruleprocessor.log_after_mapping_hook_scope(after_call=True)
+    stream.flush()
+    x = stream.getvalue().split('\n')
+
+    assert re.search('(Target groups, after).*(One)', x[6])
+    compare_attr(x[5], state['target_attributes'])
+
+
