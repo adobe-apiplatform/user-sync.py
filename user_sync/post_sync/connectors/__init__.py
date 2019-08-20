8410ed66854fdfe3c4888e669495615461a61206
Andrew Dorton <adorton@adobe.com>
2019-08-20 11:55:08 -0600

    user-defined variables/methods should not have double-underscores

--- user_sync/post_sync/connectors/__init__.py
+++ user_sync/post_sync/connectors/__init__.py
@@ -1,10 +1,10 @@
 from .sign_sync import SignConnector
 
-__CONNECTORS__ = {
+_CONNECTOR_CLASSES = {
     'sign_sync': SignConnector
 }
 
 
 def get_connector(connector_name, connector_config):
-    connector = __CONNECTORS__[connector_name]
+    connector = _CONNECTOR_CLASSES[connector_name]
     return connector(connector_config)
