a4c0bd4a57167ef2f34bd0f69e5da27c5ac4e0f6
vossen <danimaejanssen@gmail.com>
2019-08-29 16:34:07 -0500

    example mock

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -56,6 +56,47 @@ def umapi_target_info():
     return UmapiTargetInfo("")
 
 
+@mock.patch('user_sync.rules.UmapiConnectors')
+def test_log_action_summary_example(uc, rule_processor, log_stream):
+
+    class mock_am:
+        @staticmethod
+        def get_statistics():
+            return (10, 2)
+
+    connector = mock.MagicMock()
+    connector.get_action_manager.return_value = mock_am
+    uc.get_primary_connector.return_value = connector
+    uc.get_secondary_connectors.return_value = {'secondary': connector}
+
+    stream, logger = log_stream
+    rule_processor.logger = logger
+    rule_processor.log_action_summary(uc)
+
+    #result = [n.strip() for n in stream.getvalue().split('\n')]
+    result = stream.getvalue()
+
+    expected = """---------------------------- Action Summary (TEST MODE) ----------------------------
+                                  Number of directory users read: 0
+                    Number of directory users selected for input: 0
+                                      Number of Adobe users read: 0
+                     Number of Adobe users excluded from updates: 0
+              Number of non-excluded Adobe users with no changes: 0
+                                 Number of new Adobe users added: 0
+                          Number of matching Adobe users updated: 0
+                             Number of Adobe user-groups created: 0
+                      Number of Adobe users added to secondaries: 0
+                              Number of Adobe-only users removed: 0
+    Number of primary UMAPI actions sent (total, success, error): (10, 8, 2)
+  Number of secondary UMAPI actions sent (total, success, error): (10, 8, 2)
+------------------------------------------------------------------------------------
+"""
+
+    assert expected == result
+    print()
+
+
+
 @mock.patch('user_sync.helper.CSVAdapter.read_csv_rows')
 def test_stray_key_map(csv_reader, rule_processor):
     csv_mock_data = [{
