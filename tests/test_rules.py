c0b7b441719b378853da9b1f0f4df2827589e69e
Aishu <aishwaryakonduri92@gmail.com>
2019-09-11 17:01:28 -0500

    Added test_update_umapi_user to handle update_umapi_user in rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -541,3 +541,35 @@ def mock_umapi_user_data():
         {'email': 'exclude1@example.com', 'status': 'active', 'groups': ['_org_admin'],
          'username': 'exclude1@example.com',
          'adminRoles': ['org'], 'domain': 'example.com', 'country': 'US', 'type': 'federatedID'}]
+
+
+def test_update_umapi_user(rule_processor, log_stream, mock_umapi_user_data):
+    stream, logger = log_stream
+    rule_processor.logger = logger
+    umapi_info = mock.MagicMock()
+    umapi_info.get_name.return_value = None
+    umapi_connector = mock.MagicMock()
+    mock_user_key = 'federatedID,both1@example.com,'
+    mock_groups_to_add = {'added_user_group'}
+    mock_groups_to_remove = {'removed_user_group'}
+    umapi_user = mock_umapi_user_data[0]
+    umapi_user["groups"] = ["removed_user_group", "org"]
+    mock_attributes_to_update = {'firstname': 'newfirstname', 'email': 'newemail'}
+    umapi_user['username'] = 'different@example.com'
+    with mock.patch('user_sync.connector.umapi.Commands') as commands:
+        commands.return_value = mock.MagicMock()
+        rule_processor.update_umapi_user(umapi_info, mock_user_key, umapi_connector, mock_attributes_to_update,
+                                         mock_groups_to_add, mock_groups_to_remove, umapi_user)
+        commands_sent = str(umapi_connector.send_commands.call_args[0][0].method_calls)
+        commands_sent = re.sub("set\\(\\[", "{", commands_sent)
+        commands_sent = re.sub("\\]\\)", "}", commands_sent)
+        assert "update_user" in commands_sent
+        assert 'username' in commands_sent and "'firstname': 'newfirstname'" in commands_sent and "'email': 'newemail'" in commands_sent
+        assert "remove_groups({'removed_user_group'})" in commands_sent
+        assert "add_groups({'added_user_group'})" in commands_sent
+    stream.flush()
+    actual_logger_output = stream.getvalue()
+    assert 'newfirstname' in actual_logger_output
+    assert 'removed_user_group' in actual_logger_output
+    assert 'added_user_group' in actual_logger_output
+    assert umapi_user["email"] == umapi_user["username"]
