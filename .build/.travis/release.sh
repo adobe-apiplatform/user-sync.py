3da3f4ea729668597cb730fef2bcc3bfd494bd7c
Andrew Dorton <adorton@adobe.com>
2020-03-27 12:52:47 -0600

    build edition for travis

--- .build/.travis/release.sh
+++ .build/.travis/release.sh
@@ -2,7 +2,7 @@
 PY_VER=$(2>&1 python -V | sed -e 's/Python /py/g' | sed -e 's/\.//g')
 echo ${PY_VER}
 cd dist
-mv user-sync "user-sync-${TRAVIS_TAG}-${IMG}"
+mv user-sync "user-sync-${TRAVIS_TAG}${BUILD_EDITION}-${IMG}"
 cd ..
 mkdir -p release
 mv dist/* release/
