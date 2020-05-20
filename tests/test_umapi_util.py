a7feca7dce7849a6ad9306f433016fdb26a2127d
Dale Dybvig <dale.dybvig@perficient.com>
2020-05-20 12:45:00 -0500

    adjusted credman.set calls in tests to account for positional argument before keyword argument

--- tests/test_umapi_util.py
+++ tests/test_umapi_util.py
@@ -41,7 +41,7 @@ def test_make_auth_dict(umapi_config_file, private_key):
         invalid_auth_dict = make_auth_dict(name, umapi_dict_config, org_id_from_file, tech_acct_from_file, logger)
     # and if there is only the secure format it should work as long as the credential has been set
     credman = CredentialManager()
-    credman.set('make_auth_identifier', 'keydata', org_id_from_file)
+    credman.set('make_auth_identifier', 'keydata', False, org_id_from_file)
     umapi_config['enterprise']['priv_key_data'] = None
     auth_dict = make_auth_dict(name, umapi_dict_config, org_id_from_file, tech_acct_from_file, logger)
     assert auth_dict['private_key_data'] == 'keydata'
