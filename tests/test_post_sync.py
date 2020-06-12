2ac36d23f6ea84e1d29906d9a62ec1c69adb7a29
Dale Dybvig <dale.dybvig@perficient.com>
2020-06-12 13:49:46 -0500

    first pass

--- tests/test_post_sync.py
+++ tests/test_post_sync.py
@@ -1,4 +1,11 @@
+import json
+from unittest import mock
+
 import pytest
+from requests import Response
+
+from user_sync.post_sync import PostSyncConnector
+from user_sync.post_sync.connectors.sign_sync import SignClient, SignConnector
 from user_sync.post_sync.manager import PostSyncData
 
 
@@ -51,3 +58,47 @@ def test_add_remove_groups(example_user):
     delta_groups = example_user['groups'] | set(groups_add)
     delta_groups -= set(groups_remove)
     assert post_sync_data.umapi_data[None][email_id]['groups'] == delta_groups
+
+
+@mock.patch('requests.get')
+@mock.patch('user_sync.post_sync.connectors.sign_sync.client.SignClient._init')
+def test_update_sign_users(mock_client, mock_get, example_user):
+    def mock_response(data):
+        r = Response()
+        r.status_code = 200
+        r._content = json.dumps(data).encode()
+        return r
+    #     user_list = mock_response({'userInfoList': [{"userId": "123"}, {"userId": "456"}]})
+    user_list = mock_response({'userInfoList': [{"userId": "123"}]})
+    user_one = mock_response({'userStatus': 'ACTIVE', 'email': 'user@example.com'})
+    # user_two = mock_response({'userStatus': 'ACTIVE', 'email': 'user2@example.com'})
+    mock_get.side_effect = [user_list, user_one]
+    client_config = {
+                'console_org': None,
+                'host': 'api.na2.echosignstage.com',
+                'key': 'allsortsofgibberish1234567890',
+                'admin_email': 'brian.nickila@gmail.com'
+            }
+    sign_client = SignClient(client_config)
+    sign_client.api_url = "whatever"
+    # sign_users = sign_client.get_users()
+    # assert sign_users is not None
+    connector_config = {
+        'sign_orgs': [{'console_org': None,
+        'host': 'api.na2.echosignstage.com',
+        'key': 'allsortsofgibberish1234567890',
+        'admin_email': 'brian.nickila@gmail.com'}],
+        'entitlement_groups': ['group1']
+    }
+    sign_connector = SignConnector(connector_config)
+    assert isinstance(sign_connector, SignConnector)
+    example_user['email'] = 'User@example.com'
+    assert example_user['email'].lower() != example_user['email']
+    #define replacement should_sync
+    def hello(cls, umapi_user, sign_user, org_name):
+        print('x')
+        #assert sign_user not None
+    sign_connector.should_sync = hello
+    sign_connector.update_sign_users(example_user, sign_client, None)
+    # assert sign_users['user@example.com']['email'] == 'user@example.com'
+    # sign_connector.should_sync replacement method
