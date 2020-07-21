19666e85f2c5c1c635a6ebb46bb26711d698afdc
Andrew Dorton <adorton@adobe.com>
2020-07-21 15:12:07 -0600

    clean up sign config structure test

--- tests/test_sign_config.py
+++ tests/test_sign_config.py
@@ -60,24 +60,8 @@ def test_loader_attributes(sign_config_file):
 def test_config_structure(sign_config_file):
     """ensure that Sign config test fixture is structured correctly"""
     args = {'config_filename': sign_config_file}
-    config = SignConfigLoader(args)
-    assert 'sign_orgs' in config.main_config
-    assert 'primary' in config.main_config.get_dict('sign_orgs')
-    assert 'identity_source' in config.main_config
-    assert 'type' in config.main_config.get_dict('identity_source')
-    assert 'connector' in config.main_config.get_dict('identity_source')
-    assert 'user_sync' in config.main_config
-    assert 'create_users' in config.main_config.get_dict('user_sync')
-    assert 'sign_only_limit' in config.main_config.get_dict('user_sync')
-    assert 'user_management' in config.main_config
-    assert 'logging' in config.main_config
-    assert 'log_to_file' in config.main_config.get_dict('logging')
-    assert 'file_log_directory' in config.main_config.get_dict('logging')
-    assert 'file_log_name_format' in config.main_config.get_dict('logging')
-    assert 'file_log_level' in config.main_config.get_dict('logging')
-    assert 'console_log_level' in config.main_config.get_dict('logging')
-    assert 'invocation_defaults' in config.main_config
-    assert 'users' in config.main_config.get_dict('invocation_defaults')
+    _ = SignConfigLoader(args)
+    # nothing to assert here, if the config object is constructed without exceptions, then the test passes
 
 
 # NOTE: tmp_sign_connector_config and tmp_config_files are needed to prevent the ConfigFileLoader
