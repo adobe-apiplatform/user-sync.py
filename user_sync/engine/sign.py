ba93bfd48572893caa3c5749c5eaac54913c2f26
Dale Dybvig <dale.dybvig@perficient.com>
2020-08-05 11:55:39 -0500

    remove a bunch of umapi-specific parts of read_desired_user_groups

--- user_sync/engine/sign.py
+++ user_sync/engine/sign.py
@@ -172,12 +172,8 @@ class SignSyncEngine:
         return mapped_admin_roles
 
     def read_desired_user_groups(self, mappings, directory_connector):
-        # how to share the methods like self.will_process_groups? replace reference to self with umapi?
-        # or, refactor those methods into a shared directory group processor thing
-        """
-        :type mappings: dict(str, list(AdobeGroup))
-        :type directory_connector: user_sync.connector.directory.DirectoryConnector
-        """
+        # this is only the first part of the same method in engine/umapi
+        # going to make it return the modified directory users list
         self.logger.debug('Building work list...')
 
         options = self.options
@@ -202,77 +198,12 @@ class SignSyncEngine:
                 continue
             directory_user_by_user_key[user_key] = directory_user
 
-            if not self.is_directory_user_in_groups(directory_user, directory_group_filter):
-                continue
-            if not self.is_selected_user_key(user_key):
-                continue
+            # if not self.is_directory_user_in_groups(directory_user, directory_group_filter):
+            #     continue
+            # if not self.is_selected_user_key(user_key):
+            #     continue
 
-            self.filtered_directory_user_by_user_key[user_key] = directory_user
-            self.post_sync_data.update_source_attributes(user_key, directory_user['source_attributes'])
-            # self.get_umapi_info(PRIMARY_UMAPI_NAME).add_desired_group_for(user_key, None)
-
-            # set up groups in hook scope; the target groups will be used whether or not there's customer hook code
-            self.after_mapping_hook_scope['source_groups'] = set()
-            self.after_mapping_hook_scope['target_groups'] = set()
-            for group in directory_user['groups']:
-                self.after_mapping_hook_scope['source_groups'].add(group)  # this is a directory group name
-                adobe_groups = mappings.get(group)
-                if adobe_groups is not None:
-                    for adobe_group in adobe_groups:
-                        self.after_mapping_hook_scope['target_groups'].add(adobe_group.get_qualified_name())
-
-            # only if there actually is hook code: set up rest of hook scope, invoke hook, update user attributes
-            if options['after_mapping_hook'] is not None:
-                self.after_mapping_hook_scope['source_attributes'] = directory_user['source_attributes'].copy()
-
-                target_attributes = dict()
-                target_attributes['email'] = directory_user.get('email')
-                target_attributes['username'] = directory_user.get('username')
-                target_attributes['domain'] = directory_user.get('domain')
-                target_attributes['firstname'] = directory_user.get('firstname')
-                target_attributes['lastname'] = directory_user.get('lastname')
-                target_attributes['country'] = directory_user.get('country')
-                self.after_mapping_hook_scope['target_attributes'] = target_attributes
-
-                # invoke the customer's hook code
-                self.log_after_mapping_hook_scope(before_call=True)
-                exec(options['after_mapping_hook'], self.after_mapping_hook_scope)
-                self.log_after_mapping_hook_scope(after_call=True)
-
-                # copy modified attributes back to the user object
-                directory_user.update(self.after_mapping_hook_scope['target_attributes'])
-
-            for target_group_qualified_name in self.after_mapping_hook_scope['target_groups']:
-                target_group = AdobeGroup.lookup(target_group_qualified_name)
-                if target_group is not None:
-                    umapi_info = self.get_umapi_info(target_group.get_umapi_name())
-                    umapi_info.add_desired_group_for(user_key, target_group.get_group_name())
-                else:
-                    self.logger.error('Target adobe group %s is not known; ignored', target_group_qualified_name)
-
-            additional_groups = self.options.get('additional_groups', [])
-            member_groups = directory_user.get('member_groups', [])
-            for member_group in member_groups:
-                for group_rule in additional_groups:
-                    source = group_rule['source']
-                    target = group_rule['target']
-                    target_name = target.get_group_name()
-                    umapi_info = self.get_umapi_info(target.get_umapi_name())
-                    if not group_rule['source'].match(member_group):
-                        continue
-                    try:
-                        rename_group = source.sub(target_name, member_group)
-                    except Exception as e:
-                        raise error.AssertionException("Additional group resolution error: {}".format(str(e)))
-                    umapi_info.add_mapped_group(rename_group)
-                    umapi_info.add_additional_group(rename_group, member_group)
-                    umapi_info.add_desired_group_for(user_key, rename_group)
-
-        self.logger.debug('Total directory users after filtering: %d', len(self.filtered_directory_user_by_user_key))
-        if self.logger.isEnabledFor(logging.DEBUG):
-            self.logger.debug('Group work list: %s', dict([(umapi_name, umapi_info.get_desired_groups_by_user_key())
-                                                           for umapi_name, umapi_info
-                                                           in six.iteritems(self.umapi_info_by_name)]))
+        return directory_users
 
     def is_directory_user_in_groups(self, directory_user, groups):
         """
