75579613751575fcc6b3d3b146c5cf7545708651
kisarkar <kisarkar@adobe.com>
2020-08-19 20:40:14 +0530

    Resolving merge conflicts

--- tests/test_sign_engine.py
+++ tests/test_sign_engine.py
@@ -3,6 +3,8 @@ import six
 
 from user_sync.connector.directory import DirectoryConnector
 from user_sync.engine.sign import SignSyncEngine
+from user_sync.connector.connector_sign import SignConnector
+import logging
 
 
 @pytest.fixture
@@ -12,6 +14,7 @@ def example_engine(modify_root_config, sign_config_file):
     args = {'config_filename': sign_config_file}
     args['entitlement_groups'] = 'signgroup'
     args['sign_orgs'] = []
+    args['create_new_users'] = True
     return SignSyncEngine(args)
 
 
@@ -30,3 +33,26 @@ def test_load_users_and_groups(example_engine, example_user):
     dc.load_users_and_groups = dir_user_replacement
     directory_users = example_engine.read_desired_user_groups({'directory_group': 'adobe_group'}, dc)
     assert directory_users is not None
+
+
+def test_insert_new_users(example_user):
+    sign_engine = SignSyncEngine
+    sign_connector = SignConnector
+    umapi_user = example_user
+    user_roles = ['NORMAL_USER']
+    group_id = 'somemumbojumbohexadecimalstring'
+    assignment_group = 'default group'
+    insert_data = {
+            "email": umapi_user['email'],
+            "firstName": umapi_user['firstname'],
+            "groupId": group_id,
+            "lastName": umapi_user['lastname'],
+            "roles": user_roles,
+        }
+    def insert_user(insert_data):
+        pass
+    sign_connector.insert_user = insert_user
+    sign_engine.logger = logging.getLogger()
+    sign_engine.insert_new_users(sign_engine, sign_connector, umapi_user, user_roles, group_id, assignment_group)
+    assert True
+    assert insert_data['email'] == 'user@example.com'
