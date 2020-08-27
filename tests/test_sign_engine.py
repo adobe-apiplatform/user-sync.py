d0e0adf1a7f874d15163986c2e2f4436a86789ab
shasibhusan <shjena@adobe.com>
2020-08-27 22:44:01 +0530

    new scenario added for test_resolve_new_roles

--- tests/test_sign_engine.py
+++ tests/test_sign_engine.py
@@ -32,70 +32,99 @@ def test_load_users_and_groups(example_engine, example_user):
     assert directory_users is not None
 
 
-def test__groupify(example_engine,example_user):
-    processed_groups = example_engine._groupify(example_user)
+@pytest.fixture
+def input_umapi_user():
+    return {
+        'type': 'adobeID',
+        'username': 'test@adobe.com',
+        'domain': 'adobe.com',
+        'email': 'test@adobe.com',
+        'firstname': 'test',
+        'lastname': 'user',
+        'groups': {'all apps'},
+        'country': 'US'
+    }
+
+
+def test__groupify(example_engine, example_user):
+    example_user['groups'].add('sign_group One')
+    example_user['groups'].add('sign_group Two')
+    print("---------", example_user['groups'])
+    processed_groups = example_engine._groupify(example_user['groups'])
+    print(type(processed_groups))
+
     assert processed_groups is not None
 
 
-def test_get_directory_user_key(example_engine,example_user):
-    tem_user =example_engine.get_directory_user_key(example_user)
+def test_get_directory_user_key(example_engine, example_user):
+    tem_user = example_engine.get_directory_user_key(example_user)
     assert tem_user is not None
 
 
-def test_should_sync(example_engine,example_user):
-    example_user1 = example_user
-    example_user2 = example_user
-    temp_sign_user = example_engine.should_sync(example_user1,example_user2,None)
+@pytest.fixture
+def sign_user_1():
+    return {
+        'firstName': 'Dev',
+        'lastName': 'One',
+        'email': 'test1@dev-sign-02.com',
+        'company': 'Dev Sign 02',
+        'initials': 'DO',
+        'channel': 'AdobeAccountsChannel',
+        'account': 'test@xyz.com',
+        'group': 'Default Group',
+        'groupId': 'abcdef',
+        'accountType': 'GLOBAL',
+        'capabilityFlags': ['CAN_SEND', 'CAN_SIGN', 'CAN_REPLACE_SIGNER'],
+        'userStatus': 'ACTIVE',
+        'optIn': 'NO',
+        'userId': 'testiddssd',
+        'roles': ['NORMAL_USER']
+    }
+
+
+def test_should_sync(example_engine, input_umapi_user, sign_user_1):
+    temp_sign_user = example_engine.should_sync(input_umapi_user, sign_user_1, None)
     assert temp_sign_user is not None
 
 
 @pytest.fixture
 def input_resolved_roles():
-    resolved_roles =""
+    resolved_roles = ""
     return resolved_roles
+
 
 @pytest.fixture
 def input_sign_roles():
-    sign_roles =""
+    sign_roles = ""
     return sign_roles
 
 
-def test_roles_match(input_resolved_roles,input_sign_roles):
-    assert SignSyncEngine.roles_match(input_resolved_roles,input_sign_roles) == False
+def test_roles_match(input_resolved_roles, input_sign_roles):
+    assert SignSyncEngine.roles_match(input_resolved_roles, input_sign_roles) == False
     assert SignSyncEngine.roles_match("Admin", "Admin") == False
-    assert SignSyncEngine.roles_match('a','a') == True
+    assert SignSyncEngine.roles_match('a', 'a') == True
     assert isinstance("Admin", str) == True
 
 
-@pytest.fixture
-def input_umapi_user():
-     return {
-         'type': 'adobeID',
-         'username': 'test@adobe.com',
-         'domain': 'adobe.com',
-         'email': 'test@adobe.com',
-         'firstname': 'test',
-         'lastname': 'user',
-         'groups': {'all apps'},
-         'country': 'US'
-     }
+def test_resolve_new_roles(input_umapi_user):
+    role_mapping = {'all apps': {'ACCOUNT_ADMIN'}}
+    roles = SignSyncEngine.resolve_new_roles(input_umapi_user, role_mapping)
+    assert roles == ['ACCOUNT_ADMIN']
+
+    role_mapping = {'all apps': {'ACCOUNT_ADMIN', 'GROUP_ADMIN'}}
+    roles = SignSyncEngine.resolve_new_roles(input_umapi_user, role_mapping)
+    assert roles == ['ACCOUNT_ADMIN', 'GROUP_ADMIN']
+
+    role_mapping = {'all apps': {}}
+    roles = SignSyncEngine.resolve_new_roles(input_umapi_user, role_mapping)
+    assert roles == ['NORMAL_USER']
 
 
-@pytest.fixture
-def input_role_mapping():
-    return {None: {'sign_group_one': {'ACCOUNT_ADMIN'}, 'sign_group_one admin': {'GROUP_ADMIN'}}}
-
-
-def test_resolve_new_roles(input_umapi_user,input_role_mapping):
-    check_list = isinstance(SignSyncEngine.resolve_new_roles(input_umapi_user,input_role_mapping), list)
-    assert check_list == True
-
-
-def test_update_sign_users(example_engine,example_user):
+def test_update_sign_users(example_engine, example_user):
     Directory_user = "test@adobe.com"
-    org_name ="Sign test"
-    sc = SignSyncEngine.connectors ={}
-    update_user=""
-    for i in sc:
-        update_user = example_engine.update_sign_users(Directory_user,i,org_name )
+    org_name = "Sign test"
+    sc = SignSyncEngine.connectors = {}
+    update_user = ""
+    for temp in sc:
+        update_user = example_engine.update_sign_users(Directory_user, temp, org_name)
     assert update_user is not None
