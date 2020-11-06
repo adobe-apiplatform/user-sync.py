4ab86ae1cf98bd638c92ae959d14a77a78023663
jansseneda-adobe <danimaejanssen@gmail.com>
2020-11-06 16:58:53 -0600

    get_directory_group updates

--- user_sync/config/sign_sync.py
+++ user_sync/config/sign_sync.py
@@ -29,9 +29,8 @@ def config_schema() -> Schema:
         'user_management': [{
             'directory_group': Or(None, And(str, len)),
             'sign_group': Or(None, And(str, len)),
-            'admin_role': Or(None, [
-                Or(None, 'GROUP_ADMIN', 'ACCOUNT_ADMIN')
-                ]), #TODO: single "source of truth" for these options
+            Optional('group_admin', default=False): Or(bool, None),
+            Optional('account_admin', default=False): Or(bool, None)
         }],
         'logging': {
             'log_to_file': bool,
@@ -112,26 +111,49 @@ class SignConfigLoader(ConfigLoader):
         except SchemaError as e:
             raise ConfigValidationError(e.code) from e
 
-    def get_directory_groups(self) -> Dict[str, AdobeGroup]:
+    def get_directory_groups(self) -> Dict[str, dict]:
         group_mapping = defaultdict(dict)
         group_config = self.main_config.get_list_config('user_management', True)
         if group_config is None:
             return group_mapping
-        for mapping in group_config.iter_dict_configs():
+        for i, mapping in enumerate(group_config.iter_dict_configs()):
             dir_group = mapping.get_string('directory_group')
-            group_mapping[dir_group]['groups'] = []
-            group_mapping[dir_group]['roles'] = []
-            adobe_group = mapping.get_string('sign_group', True)
-            admin_roles = mapping.get_list('admin_role', True)
-            if adobe_group is not None:
-                group = AdobeGroup.create(adobe_group)
+            if dir_group not in group_mapping:
+                # Assign an ordering (priority) for sorting later, since
+                # we cannot depend on defaultdict to preserve order.
+                group_mapping[dir_group]['priority'] = i
+                group_mapping[dir_group]['groups'] = []
+                group_mapping[dir_group]['roles'] = set()
+
+            # Add all roles associated with a directory group
+            # This way, the collection or roles will be applied correctly
+            # instead of only the role associated with one group
+            if mapping.get_bool('group_admin', True):
+                group_mapping[dir_group]['roles'].add('GROUP_ADMIN')
+            if mapping.get_bool('account_admin', True):
+                group_mapping[dir_group]['roles'].add('ACCOUNT_ADMIN')
+
+            sign_group = mapping.get_string('sign_group', True)
+            if sign_group is not None:
+
+                # AdobeGroup will return the same memory instance of a pre-existing group,
+                # so we create it no matter what
+                group = AdobeGroup.create(sign_group)
+                if group is None:
+                    raise AssertionException('Bad Sign group: "{}" in directory group: "{}"'.format(sign_group, dir_group))
                 if group.umapi_name is None:
                     group.umapi_name = self.DEFAULT_ORG_NAME
-            if group is None:
-                raise AssertionException('Bad Sign group: "{}" in directory group: "{}"'.format(adobe_group, dir_group))
-            if group not in group_mapping[dir_group]:
-                group_mapping[dir_group]['groups'].append(group)
-                group_mapping[dir_group]['roles'] = admin_roles
+
+                # Note checking a memory equivalency, not group name
+                # the groups in group_mapping are stored in order of YML file - important
+                # for choosing first match for a user later
+                if group not in group_mapping[dir_group]['groups']:
+                    group_mapping[dir_group]['groups'].append(group)
+
+            # Convert to list for now to maintain compatability
+            for g in group_mapping.values():
+                g['roles'] = list(g['roles'])
+
         return dict(group_mapping)
 
     def get_directory_connector_module_name(self) -> str:
