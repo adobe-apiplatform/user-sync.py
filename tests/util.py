27a3efd50ccb5fa09cdfce9692853a7089090d0d
vossen-adobe <per38285@adobe.com>
2021-01-05 19:45:52 -0600

    update_dict no longer needed (per merge dict which is more general)

--- tests/util.py
+++ tests/util.py
@@ -1,17 +1,5 @@
-import collections
 from collections import Mapping
 from copy import deepcopy
-
-
-def update_dict(d, ks, u):
-    k, ks = ks[0], ks[1:]
-    v = d.get(k)
-    if ks and isinstance(v, collections.Mapping):
-        d[k] = update_dict(v, ks, u)
-    else:
-        d[k] = u
-    return d
-
 
 
 def make_dict(keylist, value):
