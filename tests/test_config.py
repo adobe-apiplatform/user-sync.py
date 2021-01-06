8f4b513d23368883b11ba433ed561646435192f2
vossen-adobe <per38285@adobe.com>
2021-01-05 18:07:00 -0600

    update config test fixtures

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,232 +1,195 @@
 import os
+import shutil
+
 import pytest
 import yaml
-import shutil
-from util import update_dict
-from user_sync.config import ConfigFileLoader, ConfigLoader, DictConfig
+
+import user_sync.rules
 from user_sync import flags
+from user_sync.config import ConfigFileLoader, ConfigLoader, DictConfig
+from user_sync.connector.directory import DirectoryConnector
+from user_sync.connector.directory_ldap import LDAPDirectoryConnector
 from user_sync.error import AssertionException
+from util import merge_dict, make_dict
+
+engine_defaults = user_sync.rules.RuleProcessor.default_options.copy()
 
 
-def load_ldap_config_options(args):
-    from user_sync.connector.directory import DirectoryConnector
-    from user_sync.connector.directory_ldap import LDAPDirectoryConnector
-
-    config_loader = ConfigLoader(args)
-    dc_mod_name = config_loader.get_directory_connector_module_name()
-    dc_mod = __import__(dc_mod_name, fromlist=[''])
-    dc = DirectoryConnector(dc_mod)
-    dc_config_options = config_loader.get_directory_connector_options(dc.name)
-    caller_config = DictConfig('%s configuration' % dc.name, dc_config_options)
-    return LDAPDirectoryConnector.get_options(caller_config)
+def reset_rule_options():
+    # Reset the ruleprocessor options since get_rule_options is a destructive method
+    # because it changes class variables in memory between tests
+    # If options are not reset, subsequent tests may fail
+    user_sync.rules.RuleProcessor.default_options = engine_defaults.copy()
 
 
-@pytest.fixture
-def root_config_file(fixture_dir):
-    return os.path.join(fixture_dir, 'user-sync-config.yml')
-
-
-@pytest.fixture
-def ldap_config_file(fixture_dir):
-    return os.path.join(fixture_dir, 'connector-ldap.yml')
+@pytest.fixture()
+def cleanup():
+    # Failsafe in case of failed test - resets options
+    yield
+    reset_rule_options()
 
 
 @pytest.fixture
-def umapi_config_file(fixture_dir):
-    return os.path.join(fixture_dir, 'connector-umapi.yml')
+def config_files(fixture_dir, tmpdir):
+    config_files = {
+        'ldap': 'connector-ldap.yml',
+        'umapi': 'connector-umapi.yml',
+        'root_config': 'user-sync-config.yml',
+        'extension': 'extension-config.yml',
+    }
+
+    for k, n in config_files.items():
+        shutil.copy(os.path.join(fixture_dir, n), tmpdir.dirname)
+        config_files[k] = os.path.join(tmpdir.dirname, n)
+    return config_files
 
 
 @pytest.fixture
-def extension_config_file(fixture_dir):
-    return os.path.join(fixture_dir, 'extension-config.yml')
+def modify_config(config_files):
+    def _modify_config(name, key, value, merge=True):
+        path = config_files[name]
+        conf = yaml.safe_load(open(path))
+        d = make_dict(key, value)
+        if not merge:
+            conf.update(d)
+        else:
+            merge_dict(conf, make_dict(key, value))
+        yaml.dump(conf, open(path, 'w'))
+        return path
+
+    return _modify_config
 
 
+# A shortcut for root since it is used a lot
 @pytest.fixture
-def tmp_config_files(root_config_file, ldap_config_file, umapi_config_file, tmpdir):
-    tmpfiles = []
-    for fname in [root_config_file, ldap_config_file, umapi_config_file]:
-        basename = os.path.split(fname)[-1]
-        tmpfile = os.path.join(str(tmpdir), basename)
-        shutil.copy(fname, tmpfile)
-        tmpfiles.append(tmpfile)
-    return tuple(tmpfiles)
+def modify_root_config(modify_config):
+    def _modify_root_config(key, value, merge=True):
+        return modify_config('root_config', key, value, merge)
 
-
-@pytest.fixture
-def tmp_extension_config(extension_config_file, tmpdir):
-    tmpfile = os.path.join(str(tmpdir), os.path.split(extension_config_file)[-1])
-    shutil.copy(extension_config_file, tmpfile)
-    return tmpfile
-
-
-@pytest.fixture
-def modify_root_config(tmp_config_files):
-    (root_config_file, _, _) = tmp_config_files
-
-    def _modify_root_config(keys, val):
-        conf = yaml.safe_load(open(root_config_file))
-        conf = update_dict(conf, keys, val)
-        yaml.dump(conf, open(root_config_file, 'w'))
-
-        return root_config_file
     return _modify_root_config
 
 
+# A shortcut for loading the config file
 @pytest.fixture
-def modify_ldap_config(tmp_config_files):
-    (_, ldap_config_file, _) = tmp_config_files
-
-    def _modify_ldap_config(keys, val):
-        conf = yaml.safe_load(open(ldap_config_file))
-        conf = update_dict(conf, keys, val)
-        yaml.dump(conf, open(ldap_config_file, 'w'))
-
-        return ldap_config_file
-    return _modify_ldap_config
+def default_args(cli_args, config_files):
+    return cli_args({'config_filename': config_files['root_config']})
 
 
-def test_load_root(root_config_file):
+def test_load_root(config_files):
     """Load root config file and test for presence of root-level keys"""
-    config = ConfigFileLoader.load_root_config(root_config_file)
+    config = ConfigFileLoader.load_root_config(config_files['root_config'])
     assert isinstance(config, dict)
     assert ('adobe_users' in config and 'directory_users' in config and
             'logging' in config and 'limits' in config and
             'invocation_defaults' in config)
 
 
-def test_max_adobe_percentage(modify_root_config, cli_args):
+def test_max_adobe_percentage(cleanup, cli_args, modify_root_config):
     root_config_file = modify_root_config(['limits', 'max_adobe_only_users'], "50%")
     config = ConfigFileLoader.load_root_config(root_config_file)
     assert ('limits' in config and 'max_adobe_only_users' in config['limits'] and
             config['limits']['max_adobe_only_users'] == "50%")
 
     args = cli_args({'config_filename': root_config_file})
+    reset_rule_options()  # Reset the ruleprocessor
     options = ConfigLoader(args).get_rule_options()
     assert 'max_adobe_only_users' in options and options['max_adobe_only_users'] == '50%'
 
     modify_root_config(['limits', 'max_adobe_only_users'], "error%")
+    reset_rule_options()  # Reset the ruleprocessor
     with pytest.raises(AssertionException):
         ConfigLoader(args).get_rule_options()
 
 
-def test_additional_groups_config(modify_root_config, cli_args):
+def test_additional_groups_config(cleanup, cli_args, modify_root_config):
     addl_groups = [
-        {"source": r"ACL-(.+)", "target": r"ACL-Grp-(\1)"},
-        {"source": r"(.+)-ACL", "target": r"ACL-Grp-(\1)"},
+        {
+            "source": r"ACL-(.+)",
+            "target": r"ACL-Grp-(\1)"},
+        {
+            "source": r"(.+)-ACL",
+            "target": r"ACL-Grp-(\1)"},
     ]
     root_config_file = modify_root_config(['directory_users', 'additional_groups'], addl_groups)
     config = ConfigFileLoader.load_root_config(root_config_file)
     assert ('additional_groups' in config['directory_users'] and
             len(config['directory_users']['additional_groups']) == 2)
 
-    args = cli_args({'config_filename': root_config_file})
+    args = cli_args({
+        'config_filename': root_config_file})
+
+    reset_rule_options()  # Reset the ruleprocessor
     options = ConfigLoader(args).get_rule_options()
     assert addl_groups[0]['source'] in options['additional_groups'][0]['source'].pattern
     assert addl_groups[1]['source'] in options['additional_groups'][1]['source'].pattern
 
 
-def test_directory_users_config(tmp_config_files, modify_root_config, cli_args):
-
-    # test that if connectors is not present or misspelled, an assertion exception is thrown
-    directory_users = {'not_connectors': {'ldap': 'connector-ldap.yml'}}
-    root_config_file = modify_root_config(['directory_users'], directory_users)
-    args = cli_args({'config_filename': root_config_file})
-    config_loader = ConfigLoader(args)
-    connector_name = 'ldap'
-    with pytest.raises(AssertionException):
-        config_loader.get_directory_connector_options(connector_name)
-
-
-def test_twostep_config(tmp_config_files, modify_ldap_config, cli_args):
-    (root_config_file, ldap_config_file, _) = tmp_config_files
-    modify_ldap_config(['two_steps_lookup'], {})
-
-    args = cli_args({'config_filename': root_config_file})
-
-    # test invalid "two_steps_lookup" config
-    with pytest.raises(AssertionException):
-        load_ldap_config_options(args)
-
-    # test valid "two_steps_lookup" config with "group_member_filter_format" still set
-    modify_ldap_config(['two_steps_lookup', 'group_member_attribute_name'], 'member')
-    with pytest.raises(AssertionException):
-        load_ldap_config_options(args)
-
-    # test valid "two_steps_lookup" setup
-    modify_ldap_config(['two_steps_lookup', 'group_member_attribute_name'], 'member')
-    modify_ldap_config(['group_member_filter_format'], "")
-    options = load_ldap_config_options(args)
-    assert 'two_steps_enabled' in options
-    assert 'two_steps_lookup' in options
-    assert 'group_member_attribute_name' in options['two_steps_lookup']
-    assert options['two_steps_lookup']['group_member_attribute_name'] == 'member'
-
-
-def test_adobe_users_config(tmp_config_files, modify_root_config, cli_args):
-    (root_config_file, _, _) = tmp_config_files
-    args = cli_args({'config_filename': root_config_file})
-
+def test_adobe_users_config(default_args, config_files, modify_root_config):
     # test default
-    config_loader = ConfigLoader(args)
+    config_loader = ConfigLoader(default_args)
     options = config_loader.load_invocation_options()
     assert 'adobe_users' in options
     assert options['adobe_users'] == ['all']
 
     # test default invocation
     modify_root_config(['invocation_defaults', 'adobe_users'], "mapped")
-    config_loader = ConfigLoader(args)
+    config_loader = ConfigLoader(default_args)
     options = config_loader.load_invocation_options()
     assert 'adobe_users' in options
     assert options['adobe_users'] == ['mapped']
 
     # test command line param
     modify_root_config(['invocation_defaults', 'adobe_users'], "all")
-    args = cli_args({'config_filename': root_config_file, 'adobe_users': ['mapped']})
-    config_loader = ConfigLoader(args)
+    default_args.update({
+        'config_filename': config_files['root_config'],
+        'adobe_users': ['mapped']})
+    config_loader = ConfigLoader(default_args)
     options = config_loader.load_invocation_options()
     assert 'adobe_users' in options
     assert options['adobe_users'] == ['mapped']
 
 
-def test_extension_load(tmp_config_files, modify_root_config, cli_args, tmp_extension_config, monkeypatch):
+def test_directory_users_config(modify_root_config, default_args):
+    # test that if connectors is not present or misspelled, an assertion exception is thrown
+    modify_root_config(['directory_users'], {'not_connectors': {'ldap': 'connector-ldap.yml'}}, merge=False)
+    config_loader = ConfigLoader(default_args)
+    connector_name = 'ldap'
+    with pytest.raises(AssertionException):
+        config_loader.get_directory_connector_options(connector_name)
+
+
+def test_extension_load(modify_root_config, monkeypatch, config_files, default_args, cleanup):
     """Test that extension config is loaded when config option is specified"""
     with monkeypatch.context() as m:
         m.setattr(flags, 'get_flag', lambda *a: True)
-        (root_config_file, _, _) = tmp_config_files
 
-        args = cli_args({'config_filename': root_config_file})
-        options = ConfigLoader(args).get_rule_options()
+        options = ConfigLoader(default_args).get_rule_options()
         assert 'after_mapping_hook' in options and options['after_mapping_hook'] is None
 
-        modify_root_config(['directory_users', 'extension'], tmp_extension_config)
-        options = ConfigLoader(args).get_rule_options()
+        modify_root_config(['directory_users', 'extension'], config_files['extension'])
+        options = ConfigLoader(default_args).get_rule_options()
         assert 'after_mapping_hook' in options and options['after_mapping_hook'] is not None
 
 
-def test_extension_flag(tmp_config_files, modify_root_config, cli_args, tmp_extension_config, monkeypatch):
+def test_extension_flag(modify_root_config, monkeypatch, config_files, default_args, cleanup):
     """Test that extension flag will prevent after-map hook from running"""
     with monkeypatch.context() as m:
         m.setattr(flags, 'get_flag', lambda *a: False)
 
-        (root_config_file, _, _) = tmp_config_files
-
-        args = cli_args({'config_filename': root_config_file})
-        modify_root_config(['directory_users', 'extension'], tmp_extension_config)
-        options = ConfigLoader(args).get_rule_options()
+        modify_root_config(['directory_users', 'extension'], config_files['extension'])
+        options = ConfigLoader(default_args).get_rule_options()
         assert 'after_mapping_hook' in options and options['after_mapping_hook'] is None
 
 
-def test_shell_exec_flag(tmp_config_files, modify_root_config, cli_args, monkeypatch):
+def test_shell_exec_flag(modify_root_config, monkeypatch, default_args):
     """Test that shell exec flag will raise an error if command is specified to get connector config"""
     from user_sync.connector.directory import DirectoryConnector
 
     with monkeypatch.context() as m:
         m.setattr(flags, 'get_flag', lambda *a: False)
-        (root_config_file, _, _) = tmp_config_files
 
-        args = cli_args({'config_filename': root_config_file})
         modify_root_config(['directory_users', 'connectors', 'ldap'], "$(some command)")
-        config_loader = ConfigLoader(args)
+        config_loader = ConfigLoader(default_args)
 
         directory_connector_module_name = config_loader.get_directory_connector_module_name()
         if directory_connector_module_name is not None:
@@ -234,3 +197,36 @@ def test_shell_exec_flag(tmp_config_files, modify_root_config, cli_args, monkeyp
             directory_connector = DirectoryConnector(directory_connector_module)
             with pytest.raises(AssertionException):
                 config_loader.get_directory_connector_options(directory_connector.name)
+
+
+def test_twostep_config(cli_args, config_files, modify_config):
+    def load_ldap_config_options(args):
+        config_loader = ConfigLoader(args)
+        dc_mod_name = config_loader.get_directory_connector_module_name()
+        dc_mod = __import__(dc_mod_name, fromlist=[''])
+        dc = DirectoryConnector(dc_mod)
+        dc_config_options = config_loader.get_directory_connector_options(dc.name)
+        caller_config = DictConfig('%s configuration' % dc.name, dc_config_options)
+        return LDAPDirectoryConnector.get_options(caller_config)
+
+    modify_config('ldap', ['two_steps_lookup'], {})
+    args = cli_args({
+        'config_filename': config_files['root_config']})
+
+    # test invalid "two_steps_lookup" config
+    with pytest.raises(AssertionException):
+        load_ldap_config_options(args)
+
+    # test valid "two_steps_lookup" config with "group_member_filter_format" still set
+    modify_config('ldap', ['two_steps_lookup', 'group_member_attribute_name'], 'member')
+    with pytest.raises(AssertionException):
+        load_ldap_config_options(args)
+
+    # test valid "two_steps_lookup" setup
+    modify_config('ldap', ['two_steps_lookup', 'group_member_attribute_name'], 'member')
+    modify_config('ldap', ['group_member_filter_format'], "")
+    options = load_ldap_config_options(args)
+    assert 'two_steps_enabled' in options
+    assert 'two_steps_lookup' in options
+    assert 'group_member_attribute_name' in options['two_steps_lookup']
+    assert options['two_steps_lookup']['group_member_attribute_name'] == 'member'
