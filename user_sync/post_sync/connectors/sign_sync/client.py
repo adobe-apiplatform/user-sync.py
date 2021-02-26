c59e2c8a956e4f07ca4aeeeb47ee769dc7eaf3af
vossen-adobe <per38285@adobe.com>
2021-02-26 17:28:41 -0600

    clean up, fix hanging

--- user_sync/post_sync/connectors/sign_sync/client.py
+++ user_sync/post_sync/connectors/sign_sync/client.py
@@ -26,9 +26,11 @@ class SignClient:
         self.groups = None
         self.max_sign_retries = 3
         self.sign_timeout = 120
-        self.concurrency_limit = 5
+        self.concurrency_limit = 10
         self.logger = logging.getLogger(self.logger_name())
         logging.getLogger("urllib3").setLevel(logging.WARNING)
+        self.loop = asyncio.get_event_loop()
+        self.users = {}
 
     def _init(self):
         self.api_url = self.base_uri()
@@ -54,7 +56,6 @@ class SignClient:
             }
         return {
             'Access-Token': self.key,
-            # 'Connection': 'close',
         }
 
     def header_json(self):
@@ -66,7 +67,6 @@ class SignClient:
         json_headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Connection': 'close',
         }
         json_headers.update(self.header())
         return json_headers
@@ -97,28 +97,30 @@ class SignClient:
 
         return result.json()[access_point_key] + endpoint
 
-    async def call_with_async(self, method, url, header, data=None):
+    async def call_with_retry_async(self, method, url, header, data=None, session=None):
         """
         Call manager with exponential retry
         :return: Response <Response> object
         """
         retry_nb = 1
         waiting_time = 20
+        close = session is None
+        session = session or aiohttp.ClientSession()
+        session.headers.update(header)
         while True:
             try:
                 waiting_time *= 3
                 self.logger.debug('Attempt {} to call: {}'.format(retry_nb, url))
-                async with aiohttp.ClientSession(headers=header) as session:
-                    async with session.request(method=method, url=url, data=data or {}) as r:
-                        if r.status >= 500:
-                            raise Exception('{}, Headers: {}'.format(r.status, r.headers))
-                        elif r.status == 429:
-                            raise Exception('{} - too many calls. Headers: {}'.format(r.status, r.headers))
-                        elif r.status > 400 and r.status < 500:
-                            self.logger.critical(' {} - {}. Headers: {}'.format(r.status, r.reason, r.headers))
-                            raise AssertionException('')
-                        body = await r.json()
-                        return body
+                async with session.request(method=method, url=url, data=data or {}) as r:
+                    if r.status >= 500:
+                        raise Exception('{}, Headers: {}'.format(r.status, r.headers))
+                    elif r.status == 429:
+                        raise Exception('{} - too many calls. Headers: {}'.format(r.status, r.headers))
+                    elif r.status > 400 and r.status < 500:
+                        self.logger.critical(' {} - {}. Headers: {}'.format(r.status, r.reason, r.headers))
+                        raise AssertionException('')
+                    body = await r.json()
+                    return body
             except Exception as exp:
                 self.logger.warning('Failed: {}'.format(exp))
                 if retry_nb == (self.max_sign_retries + 1):
@@ -126,75 +128,66 @@ class SignClient:
                 self.logger.warning('Waiting for {} seconds'.format(waiting_time))
                 time.sleep(waiting_time)
                 retry_nb += 1
+            finally:
+                if close:
+                    await session.close()
 
     def call_with_retry_sync(self, method, url, header, data=None):
-
-        # loop will execute a single synchronous call, but sharing code with the async retry method
-        loop = asyncio.get_event_loop()
-        res = loop.run_until_complete(self.call_with_async(method, url, header, data=data or {}))
-        return res
+        """
+        Need to define this method, so that it can be called outside async context
+        loop will execute a single synchronous call, but sharing code with the async retry method
+        """
+        return self.loop.run_until_complete(self.call_with_retry_async(method, url, header, data=data or {}))
 
     def get_users(self):
         """
-        Get list of all users from Sign (indexed by email address)
-        :return: dict()
+        Need to define this method, so that it can be called outside async context
+        return dict so as to comply with existing code calling this function
+        *prefer instance var to awaiting coroutine results as we cannot
+        guarantee they will be user obj
         """
+        self.loop.run_until_complete(self.get_users_async())
+        return self.users
 
+    async def get_users_async(self):
+        """
+        Get list of all users from Sign (indexed by email address)
+        """
         if self.api_url is None or self.groups is None:
             self._init()
 
-        header = self.header()
+        headers = self.header()
         users_url = self.api_url + 'users'
         self.logger.info('getting list of all Sign users')
-
-        # Synchronous call to get user list, which will be distributed to asyncio
-        users_res = self.call_with_retry_sync('GET', users_url, header)
-
-        # Also define self.users = {} to use as receptacle for users.  Important to
-        # use self context because we cannot easily return values from our get calls
-        # Instead, they concurrently add to the users dict, and we will return it
-        # at the end when it is finished
-        calls = []
-        self.users = {}
 
         # Semaphore specifies number of allowed calls at one time
         sem = asyncio.Semaphore(value=self.concurrency_limit)
 
-        # prepare a list of calls to make * Note: calls are prepared by using call
-        # syntax (eg, func() and not func), but they will not be run until executed
-        # by the loop
-        for user_id in map(lambda u: u['userId'], users_res['userInfoList']):
-            calls.append(self.get_sign_user(sem, user_id, header))
+        # We must use only 1 session, else will hang
+        async with aiohttp.ClientSession() as session:
 
-        # loop will execute the tasks
-        # wait for all calls to finish
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.wait(calls))
+            # Get the user list
+            user_list = await self.call_with_retry_async('GET', users_url, headers, session=session)
 
-        # return so as to comply with existing code calling this function
-        # *prefer instance var to awaiting coroutine results as we cannot
-        # guarantee they will be user obj
-        return self.users
+            # prepare a list of calls to make * Note: calls are prepared by using call
+            # syntax (eg, func() and not func), but they will not be run until executed by the wait
+            calls = [self.get_sign_user(sem, u['userId'], headers, session) for u in user_list['userInfoList']]
+            await asyncio.wait(calls)
 
-    # Note the async def - this function can only be executed in asyncio context
-    async def get_sign_user(self, semaphore, user_id, header):
+    async def get_sign_user(self, semaphore, user_id, header, session):
 
         # This will block the method from executing until a position opens
-        await semaphore.acquire()
-
-        user_url = self.api_url + 'users/' + user_id
-        user = await self.call_with_async('GET', user_url, header)
-        if user['userStatus'] != 'ACTIVE':
-            return
-        if user['email'] == self.admin_email:
-            return
-        user['userId'] = user_id
-        user['roles'] = self.user_roles(user)
-        self.users[user['email']] = user
-        self.logger.debug('retrieved user details for Sign user {}'.format(user['email']))
-
-        # Release the worker back to pool
-        semaphore.release()
+        async with semaphore:
+            user_url = self.api_url + 'users/' + user_id
+            user = await self.call_with_retry_async('GET', user_url, header, session=session)
+            if user['userStatus'] != 'ACTIVE':
+                return
+            if user['email'] == self.admin_email:
+                return
+            user['userId'] = user_id
+            user['roles'] = self.user_roles(user)
+            self.users[user['email']] = user
+            self.logger.debug('retrieved user details for Sign user {}'.format(user['email']))
 
     def get_groups(self):
         """
