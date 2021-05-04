f908d8a9de2917dd359e005481f8716c7dc4339f
shasibhusan <shjena@adobe.com>
2021-05-05 01:42:48 +0530

    Revert "update test_client.py"
    This reverts commit bc7b5e5d4b54b05e4636223774c7729d7f07b7e6.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,7 @@
 from unittest import mock
 
 import pytest
-
+from mock import MagicMock
 from sign_client.client import SignClient
 
 
@@ -16,7 +16,6 @@ class MockResponse:
     def json(self):
         return self.body
 
-
 @pytest.fixture
 def signclient_example():
     return SignClient(host='api.echosignstage.com',
@@ -26,39 +25,54 @@ def signclient_example():
 
 def test_base_uri(signclient_example):
     with mock.patch('sign_client.client.requests') as mocked_requests:
-        mocked_requests.get.return_value = MockResponse(body={'api_access_point': 'api_test_value/'})
+        mocked_requests.get.return_value = MockResponse(
+            body={
+                'api_access_point':'api_test_value/'
+            }
+        )
         result = signclient_example.base_uri()
         assert result == 'api_test_value/api/rest/v5/'
 
 def test_get_users(signclient_example):
-    userInfoList = {"userInfoList": [{"groupId": "OZR8L8ZDql6m5","email": "user@example.com",
-                                      "fullNameOrEmail": "test user","userId": "l3WbzoofzjCmEH5gZcX7"
-                                      }]}
-
-    userdetail = {"email": "user1@example.com", "group": "Default Group","roles": "ACCOUNT_ADMIN",  "userStatus": "ACTIVE"}
+    mock_get = MagicMock()
     with mock.patch('sign_client.client.requests') as mocked_requests:
-        mocked_requests.get.side_effect = [MockResponse(body=userInfoList), MockResponse(body=userdetail)]
+        mocked_requests.get.return_value = MockResponse(
+            body={"userInfoList": [
+                    {
+                        "groupId": "3AAABLZtkPdAH9sz1KbI8GSydrljH2bmhtjOwhNHD9NnnaEZbGyzjGWiiDkEOGzzF5AVZDCXyBwDiebPyEY-OZR8L8ZDql6m5",
+                        "email": "user@example.com",
+                        "fullNameOrEmail": "test user",
+                        "userId": "3AAABLZtkPdBf9rlRchWj-U8cHaklFlsBIuy2GDZa0x44_dBwCkS8r7vwianWRpwC07JSA6f-UBx6haKX9Nf1z_ZccMMiMwpV"
+                    },
+                    {
+                        "groupId": "3AAABLZtkPdAH9sz1KbI8GSydrljH2bmhtjOwhNHD9NnnaEZbGyzjGWiiDkEOGzzF5AVZDCXyBwDiebPyEY-OZR8L8ZDql6m5",
+                        "email": "user@example.com",
+                        "fullNameOrEmail": "test user",
+                        "company": "Dev Sign 02",
+                        "userId": "3AAABLZtkPdCXic1dNAXpxblxIO2sR0pnbOWYGBhzhdNwON8NbKJoj2IPbClomEdRzNhQJrx64zZjl3WbzoofzjCmEH5gZcX7"
+                    }
+                ]
+            }
+        )
         sc = signclient_example
-        sc.api_url = ''
-        sc.groups = []
-        result = sc.get_users()
-        assert result['user1@example.com']['email'] == 'user1@example.com'
-        assert result['user1@example.com']['group'] == 'Default Group'
-        assert result['user1@example.com']['roles'] == 'ACCOUNT_ADMIN'
-        assert result['user1@example.com']['userStatus'] == 'ACTIVE'
+        sc.api_url =''
+        sc.groups =[]
+#        result = sc.get_users()
+
 
 def test_get_groups(signclient_example):
+    mock_get = MagicMock()
     with mock.patch('sign_client.client.requests') as mocked_requests:
         mocked_requests.get.return_value = MockResponse(
             body={'groupInfoList': [{
-                'groupId': 'OZR8L8ZDql6m5',
-                'groupName': 'Default Group'}, {
-                'groupId': 'LBQBZLPrDk7Pn',
-                'groupName': 'Group 1'}, {
-                'groupId': 'tin31laZIb36L',
-                'groupName': 'Group 2'}]}
+                                   'groupId': 'OZR8L8ZDql6m5',
+                                   'groupName': 'Default Group'}, {
+                                   'groupId': 'LBQBZLPrDk7Pn',
+                                   'groupName': 'Group 1'}, {
+                                   'groupId': 'tin31laZIb36L',
+                                   'groupName': 'Group 2'}]}
         )
         sc = signclient_example
-        sc.api_url = ""
+        sc.api_url =""
         result = sc.get_groups()
         assert result == {'default group': 'OZR8L8ZDql6m5', 'group 1': 'LBQBZLPrDk7Pn', 'group 2': 'tin31laZIb36L'}
