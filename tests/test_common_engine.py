2ad70c4d68e23a0421dee6b6d748632c9f2b93f2
Mariana <marianabonish88@gmail.com>
2021-08-09 20:48:52 -0500

    tests

--- tests/test_common_engine.py
+++ tests/test_common_engine.py
@@ -1,10 +1,22 @@
 from user_sync.engine.common import AdobeGroup
-import pytest
 
 class TestAdobeGroup():
 
-    # def test_get_qualified_name(self):
-    #     assert self.group_name
+    def test_get_qualified_name(self):
+        #standard test
+        self.umapi_name = None
+        self.group_name = 'group_name'
+        result = AdobeGroup.get_qualified_name(self)
+        assert result == ('group_name')
+
+        #umapi_name variation test
+        GROUP_NAME_DELIMITER = '::'
+        PRIMARY_TARGET_NAME ='not_umapi_name'
+        self.umapi_name = 'umapi_name'
+        self.group_name = 'group_name'
+        result = AdobeGroup.get_qualified_name(self)
+        assert result == ("umapi_name::group_name")
+
 
     def test_parse(self):
         result = AdobeGroup._parse('qualified_name')
