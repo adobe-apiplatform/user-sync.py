6402332e734365417771f7757efde0a6aca992ed
vossen-adobe <per38285@adobe.com>
2021-08-25 17:52:13 -0500

    fixed

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,16 +1,9 @@
-import pytest
-import six
+from unittest.mock import MagicMock
 
-from user_sync.config import ConfigLoader
-from user_sync.connector.directory import DirectoryConnector
+import pytest
+
 from user_sync.rules import RuleProcessor
 
-
-@pytest.fixture
-def example_engine(default_args):
-    config = ConfigLoader(default_args)
-    rule_config = config.get_rule_options()
-    return RuleProcessor(rule_config)
 
 @pytest.fixture
 def example_user():
@@ -34,53 +27,27 @@ def example_user():
     }
 
 
-def test_read_desired_user_groups(example_engine, example_user, test_resources):
+def test_skip_user(example_user):
+    # Fake directory connector
+    dc = MagicMock()
+    dc.load_users_and_groups.return_value = [example_user]
 
-    dc = DirectoryConnector
-    user = {'user@example.com': example_user}
+    def run_extension(text):
+        rp = RuleProcessor({})
+        after_mapping_hook_text = text
+        rp.options['after_mapping_hook'] = compile(
+            after_mapping_hook_text, '<per-user after-mapping-hook>', 'exec')
+        rp.read_desired_user_groups({}, dc)
+        return rp
 
-    def dir_user_replacement(groups, extended_attributes, all_users):
-        return six.itervalues(user)
+    state = run_extension('pass')
+    user_key = state.get_directory_user_key(example_user)
+    assert user_key in state.directory_user_by_user_key
+    assert user_key in state.filtered_directory_user_by_user_key
+    assert state.skipped_user_count == 0
 
-    dc.load_users_and_groups = dir_user_replacement
-    mappings = {
-        'Test Group Admins 1': {
-            'priority': 4,
-            'roles': {'GROUP_ADMIN'},
-            'groups': []
-        },
-        'Test Group Admins 2': {
-            'priority': 1,
-            'roles': {'ACCOUNT_ADMIN'},
-            'groups': []
-        },
-    }
-    assert example_engine.read_desired_user_groups({}, dc) is None
-    assert example_engine.read_desired_user_groups(mappings, dc) is None
-
-
-def test_skip_user(example_engine,example_user,test_resources):
-
-    user = {'user@example.com': example_user}
-
-    def dir_user_replacement(groups, extended_attributes, all_users):
-        return six.itervalues(user)
-    dc = DirectoryConnector
-    dc.load_users_and_groups = dir_user_replacement
-    mappings = {
-        'Test Group Admins 1': {
-            'priority': 4,
-            'roles': {'GROUP_ADMIN'},
-            'groups': []
-        },
-        'Test Group Admins 2': {
-            'priority': 1,
-            'roles': {'ACCOUNT_ADMIN'},
-            'groups': []
-        },
-    }
-
-    after_mapping_hook_text = 'skip_user'
-    example_engine.options['after_mapping_hook'] = compile(after_mapping_hook_text, '<per-user after-mapping-hook>', 'exec')
-    assert example_engine.read_desired_user_groups({}, dc) is None
-    assert example_engine.read_desired_user_groups(mappings, dc) is None
+    state = run_extension('skip_user()')
+    user_key = state.get_directory_user_key(example_user)
+    assert user_key not in state.directory_user_by_user_key
+    assert user_key not in state.filtered_directory_user_by_user_key
+    assert state.skipped_user_count == 1
