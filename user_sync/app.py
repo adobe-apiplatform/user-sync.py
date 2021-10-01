144a307cb2e0a0ae4fae6a03769e1a59d483f47a
Andrew Dorton <adorton@adobe.com>
2021-10-01 09:37:43 -0600

    strip newlines from OS release file

--- user_sync/app.py
+++ user_sync/app.py
@@ -108,7 +108,7 @@ def info():
         click.echo("OS Release Info:")
         with open('/etc/os-release') as f:
             for l in f:
-                click.echo(f"  {l}")
+                click.echo(f"  {l.strip()}")
     click.echo("Packages:")
     pkg_meta_file = resource.get_resource('pkg_meta.json')
     with open(pkg_meta_file) as f:
